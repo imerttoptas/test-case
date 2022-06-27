2f7353f95169f3527b5b60209723a0237c22c43f
imerttoptas <toptasi20@itu.edu.tr>
2022-06-27 20:42:27 +0300

    LevelManager+InputManager

---
+++ Test-Case/Assets/Scripts/InputManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public static int gridsize;
+
+    public void ReadStringInput(string s)
+    {
+        gridsize = (int)Convert.ToInt32(s);
+    }
+}
